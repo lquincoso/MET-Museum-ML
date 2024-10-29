54c2b7f2465c4828082b5120331685d54fd616ce
doapo <124337121+doapo@users.noreply.github.com>
2024-10-29 13:54:31 -0400

    Artwork details page set up

--- front-end/web-front/src/pages/Artwork-Details.js
+++ front-end/web-front/src/pages/Artwork-Details.js
@@ -5,16 +5,17 @@ import "./Artwork-Details.css";
 const ART_DETAILS = {
   image: "Artwork Image Here",
   title: "Artwork Name Here",
-  location: "On View At (location)",
+  location: "(location)",
   artist: "Artist Name",
   date: "Date Created",
   period: "Period (if applicable)",
   culture: "Culture (if applicable)",
   medium: "Medium Used",
   about: "About the Artwork",
-  resources: "Resources for Further Reading",
-  rating: 0,
+  resources: "Resources for Further Reading"
 };
+
+const TEMP_RATING = 3;
 
 function ArtworkDetails() {
   return (
@@ -27,13 +28,13 @@ function ArtworkDetails() {
               key={star}
               size={32}
               className={`${
-                star <= ART_DETAILS.rating
+                star <= TEMP_RATING
                   ? star === 2
-                    ? "fill-red-500 text-red-500"
+                    ? "stars"
                     : star === 3
-                    ? "fill-yellow-400 text-yellow-400"
-                    : "fill-green-500 text-green-500"
-                  : "text-gray-300"
+                    ? "stars"
+                    : "stars"
+                  : "stars"
               }`}
             />
           ))}
@@ -41,7 +42,10 @@ function ArtworkDetails() {
       </div>
       <div className="right-container">
         <div className="artwork-title"> {ART_DETAILS.title}</div>
-        <div className="artwork-location"> {ART_DETAILS.location} </div>
+        <div className="artwork-location">
+          {" "}
+          On View At: {ART_DETAILS.location}{" "}
+        </div>
         <div className="artwork-info">
           {[
             { label: "Artist", value: ART_DETAILS.artist },
@@ -50,18 +54,18 @@ function ArtworkDetails() {
             { label: "Culture", value: ART_DETAILS.culture },
             { label: "Medium", value: ART_DETAILS.medium },
           ].map(({ label, value }) => (
-            <div key={label}>
-              <div className="art-info">{label}:</div>
-              <div>{value}</div>
+            <div key={label} className="art-info-item">
+              <div className="art-info-label">{label}:</div>
+              <div className="art-info-value">{value}</div>
             </div>
           ))}
         </div>
         <div className="about-container">
-          <div className="about-title">About</div>
+          <div className="art-info-label">About: </div>
           <div className="about-text">{ART_DETAILS.about}</div>
         </div>
         <div className="resources-container">
-          <div className="resources-title">Resources</div>
+          <div className="art-info-label">Resources: </div>
           <div className="resources-text">{ART_DETAILS.resources}</div>
         </div>
       </div>
